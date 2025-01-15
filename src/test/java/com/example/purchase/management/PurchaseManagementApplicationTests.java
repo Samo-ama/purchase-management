d51678e495e407851e3feb2627a3b6ba9414d4a3
Samo <ebtisamalaama@gmail.com>
2025-01-15 12:50:47 +0300

    Initial commit: Project setup with Spring Boot 3.2.0

---
+++ src/test/java/com/example/purchase/management/PurchaseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.purchase.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
