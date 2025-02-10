58c53d53d61183a311a28ea1d55f623e54641d8d
baylasan-daoud <kayaa28102100@gmail.com>
2025-02-10 23:37:10 +0300

    Create ProductRepositoryTest: FindById, Delete, and FindAll

---
+++ src/test/java/com/example/purchase/management/repository/ProductRepositoryTest.java
@@ -0,0 +1,80 @@
+package com.example.purchase.management.repository;
+
+import com.example.purchase.management.entity.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(
+        connection = EmbeddedDatabaseConnection.H2
+)
+
+class ProductRepositoryTest {
+    @Autowired
+    private ProductRepository productRepository;
+    private Product product1;
+    private Product product2;
+
+    private List<Product> all;
+
+    @BeforeEach
+    void setUp() {
+        product1 = new Product();
+        product1.setName("Dell Lap");
+        product1.setPrice(1000.00);
+
+        product2 = new Product();
+        product1.setName("HP Lap");
+        product1.setPrice(1200.00);
+
+        all = productRepository.saveAll(List.of(product1, product2));
+    }
+
+    @Test
+    public void findById_ProductExists_ShouldReturnProduct(){
+        Optional<Product> firstProduct = productRepository.findById(product1.getId());
+
+        assertTrue(firstProduct.isPresent());
+        assertEquals(product1.getId() , firstProduct.get().getId());
+    }
+
+    @Test
+    public void findById_ProductNotExist_ShouldReturnEmpty(){
+        Optional<Product> notExistedProduct = productRepository.findById(0L);
+
+        assertTrue(notExistedProduct.isEmpty());
+    }
+
+    @Test
+    public void findAll_ShouldReturnAllProducts(){
+        int expectedSize = all.size();
+
+        List<Product> products = productRepository.findAll();
+        int actualSize = all.size();
+
+        assertNotNull(products);
+        assertEquals(all.size(), products.size());
+        assertEquals(all.get(0), products.get(0));
+        assertEquals(all.get(expectedSize-1), products.get(actualSize-1));
+    }
+
+    @Test
+    public void delete_ProductExists_ShouldRemoveProduct() {
+        int productsSizeBefore = all.size();
+        productRepository.delete(product2);
+
+        Optional<Product> foundProduct = productRepository.findById(product2.getId());
+        assertFalse(foundProduct.isPresent());
+        assertEquals(productsSizeBefore - 1 ,productRepository.findAll().size());
+    }
+
+}
